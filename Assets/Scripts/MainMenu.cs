48e0777ad07b48334b21b703314685f5ced896d6
mohamed alaa <m-aa2001@hotmail.com>
2025-01-02 23:54:35 +0200

    added layer on city env

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,11 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void Play()
     {
-<<<<<<< Updated upstream
-        SceneManager.LoadSceneAsync("Level1");
-=======
         int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
         SceneManager.LoadScene(nextSceneIndex);
->>>>>>> Stashed changes
     }
 }
